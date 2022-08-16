a7ea1f0e5de8a72dd4c26556c7067e1bd1b562e2
javim7 <rjmombiela@gmail.com>
2022-08-16 14:16:40 -0600

    cambios en supermercado

--- client/src/Components/Content/Supermercados1/Supermercados1.jsx
+++ client/src/Components/Content/Supermercados1/Supermercados1.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Divider, Stepper, Button, Group } from '@mantine/core'
+import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from 'firebase/firestore'
 
 function Supermercados1() {
+
+	const [supermercados, setSupermercados] = useState([]);
+
+	useEffect(() => {
+
+		const referenciaSupermercado = collection(db, 'supermercados')
+
+		const reatrerSupermercado = async () => {
+			const datos = await getDocs(referenciaSupermercado)
+			setSupermercados(datos.docs.map(doc => ({ ...doc.data(), id: doc.id })))
+		}
+		reatrerSupermercado()
+
+	}, []);
+
+
   return (
     <div className='SupermercadosContainer'>
       <Stepper active={1} color="yellow" size="lg" breakpoint="sm" style={{ marginRight: '45px', fontFamily: 'Overpass, sans-serif' }}>
